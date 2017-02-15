5c30487cf9e2b8ac9861f86bc227b347bfe3c31f
cassi fromer <cassi fromer>
2017-02-15 12:08:53 +0200

    files

---
+++ contact-process.php
@@ -0,0 +1,21 @@
+<?
+$name = trim(stripslashes($_POST['your-name']));
+$email = trim(stripslashes($_POST['your-email']));
+$message = trim(stripslashes($_POST['your-message']));
+
+$recipient = "chazfromer@gmail.com";
+$email_from = $name." <".$email.">";
+
+$body = "Name: ".$name;
+$body .= "\nEmail: ".$email.;
+$body .= "\nMessage: ".$message;
+
+$success = mail($recipient, "message from pilatesinspire", $body, "From: ".$email_from);
+
+if ($success){
+    echo"message sent!";
+}else{
+    echo "didnt work";
+}
+
+?>
