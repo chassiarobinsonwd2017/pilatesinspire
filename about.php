5bef361cfa2681946b3d55ffce4ceb2283d7f3e9
cassi fromer <cassi fromer>
2017-03-14 22:13:40 +0200

    v

--- about.php
+++ about.php
@@ -10,7 +10,18 @@
         <div class = "subheading"><h2>About us</h2></div>
               <p>We are a happy, relaxed studio and that is reflected through all our classes and the caring, empathetic nature of all our teachers. We love the sense of community that brings. We have a range of classes a week in Pilates and pre and post-natal, and lots of private one to one sessions. The quality and purity of our teaching is fundamental to us, and our teachers are highly qualified and experienced, and passionate about the benefits and changes that you will discover with their guidance.  Do call us for advice and we hope to meet you soon.</p>
         </div>
+<div class="col-sm-6" >
+        <div class = "subheading"><h2>Location</h2></div>
+    <p>Our spacious and beautiful studio is located on Rav Shimon Agassi street, Har nof, Jerusalem.Classes are held at different locations around the city.</p>
+<img src = "https://maps.googleapis.com/maps/api/staticmap?center=18+Ha-Rav+Shim'on+Agasi+Street, Jerusalem, Israel, 91346&zoom=17&size=600x300&maptype=roadmap
+&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318
+&markers=color:red%7Clabel:C%7C40.718217,-73.998284
+&key=AIzaSyDyUTd7Ev8mgUIodngZ3Mi0a3I9fJPctfs ">
+    </div>
+</div>
 
 
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
 <? include("footer.html"); ?>
