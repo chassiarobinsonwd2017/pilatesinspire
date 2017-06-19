7825f339347a21920a20eb7051b9e240f5494181
cassi fromer <cassi fromer>
2017-06-19 17:01:31 +0300

    updates

--- schedule.php
+++ schedule.php
@@ -1,2 +1,108 @@
 <? include("header.html"); ?>
+
+<div class ="heading"><div><img src ="assets/l.jpg"></div><h1>Schedule</h1></div>
+<table>
+    <tr>
+        <th>Time</th>
+        <th>Sunday</th>
+        <th>Monday</th>
+        <th>Tuesday</th>
+        <th>Wednesday</th>
+        <th>Thursday</th>
+        <th>Friday</th>
+    </tr>
+    <tr>
+        <th>9:00-10:00am</th>
+        <th>Pilates-Sara</th>
+        <th>Pilates-Miri</th>
+        <th>Pilates Pre-natal-Linda</th>
+        <th></th>
+        <th>Pilates-Linda</th>
+        <th></th>
+
+    </tr>
+    <tr>
+        <th>11:00-12:00pm</th>
+        <th>Pilates-Michal</th>
+        <th>Pilates-Leah</th>
+        <th>Pilates Elderly-Daniela</th>
+        <th>Pilates Pre-natal-Linda</th>
+        <th>Pilates-Sara</th>
+        <th>Pilates-Jane</th>
+
+    </tr>
+    <tr>
+        <th>12:00-1:00pm</th>
+        <th></th>
+        <th>Pilates-Racheli</th>
+        <th></th>
+        <th></th>
+        <th>Pilates-Leah</th>
+        <th>Pilates-Sara</th>
+
+    </tr>
+    <tr>
+        <th>1:00-2:00pm</th>
+        <th></th>
+        <th></th>
+        <th>Pilates Elderly-Daniela</th>
+        <th></th>
+        <th>Pilates-Daniela</th>
+        <th>Pilates-Sara</th>
+
+    </tr>
+     <tr>
+        <th>2:00-3:00pm</th>
+        <th>Pilates Pre-natal-Linda</th>
+        <th></th>
+        <th>Pilates-Racheli</th>
+        <th>Pilates Elderly-Daniela</th>
+        <th>Pilates-Daniela</th>
+        <th>Pilates-Sara</th>
+
+    </tr>
+     <tr>
+        <th>3:00-4:00pm</th>
+        <th>Pilates Racheli</th>
+        <th></th>
+        <th></th>
+        <th>Pilates-Miri</th>
+        <th>Pilates Elderly-Daniela</th>
+        <th>Pilates-Daniela</th>
+
+    </tr>
+     <tr>
+        <th>4:00-5:00pm</th>
+        <th></th>
+        <th></th>
+        <th>Pre-natal-Yael</th>
+        <th>Pilates-Linda</th>
+        <th></th>
+        <th>Pilates Elderly-Daniela</th>
+
+    </tr>
+
+
+
+
+
+
+
+
+
+
+</table>
+
+
+
+
+
+
+
+
+
+
+
+
+
 <? include("footer.html"); ?>
